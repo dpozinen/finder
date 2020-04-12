95d1579577694751469a6050959a87827dc2074a
dpozinen <dpozinen@gmail.com>
2020-04-12 18:43:56 +0300

    make controller use the finder service

--- src/main/java/finder/controller/FinderController.java
+++ src/main/java/finder/controller/FinderController.java
@@ -1,22 +1,36 @@
 package finder.controller;
 
-import finder.model.Page;
-import org.springframework.beans.factory.annotation.Autowired;
+import finder.service.FinderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
 
 @EnableScheduling
 @Controller
 public class FinderController {
 
-	@Autowired
-	private SimpMessagingTemplate template;
+	private final SimpMessagingTemplate template;
+	private final FinderService service;
+
+	public FinderController(SimpMessagingTemplate template, FinderService service) {
+		this.template = template;
+		this.service = service;
+	}
 
 	@Scheduled(fixedRate = 4000)
 	public void refresh() {
-		template.convertAndSend("/updates", new Page(1L, "1", 1L, 2));
+		template.convertAndSend("/updates", service.update());
 	}
 
+	@PostMapping("/finder/query")
+	public void query(@RequestBody Map<String, String> form) {
+		service.find(form);
+	}
 }
