abe81d3af30c6299e9dd50beac4baf3124fdd640
dpozinen <dpozinen@gmail.com>
2020-04-20 11:02:39 +0300

    add Job status

--- src/main/java/finder/model/Job.java
+++ src/main/java/finder/model/Job.java
@@ -1,5 +1,6 @@
 package finder.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import finder.service.Finder;
 import finder.service.Pause;
 import lombok.Data;
@@ -15,32 +16,43 @@ public @Data class Job implements Serializable {
 
 	@Id
 	private String id;
+	@JsonIgnore
 	private transient final Finder finder;
 	private final Set<Page> pages = new HashSet<>();
-	private final Pause pause = new Pause();
+	@JsonIgnore
+	private transient final Pause pause = new Pause();
+	private volatile Status status;
 
 	public Job(Finder.Input input) {
 		this.finder = new Finder(input, pause, pages);
+		this.status = Status.QUEUED;
 	}
 
 	public void run() {
 		finder.find();
+		status = Status.DONE;
 	}
 
 	public void pause() {
+		status = Status.PAUSED;
 		pause.pause();
 	}
 
 	public void play() {
+		status = Status.RUNNING;
 		pause.resume();
 	}
 
 	public void stop() {
 		finder.stop();
+		status = Status.CANCELLED;
 	}
 
 	public void reset() {
 		stop();
 	}
 
+	enum Status {
+		DONE, PAUSED, RUNNING, QUEUED, CANCELLED
+	}
 }
