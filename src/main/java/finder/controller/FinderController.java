dfc2ccb4661d618062e6d48c40ab65fc8efa3161
dpozinen <dpozinen@gmail.com>
2020-04-20 11:22:58 +0300

    add logic to get job by id

--- src/main/java/finder/controller/FinderController.java
+++ src/main/java/finder/controller/FinderController.java
@@ -1,5 +1,6 @@
 package finder.controller;
 
+import finder.model.Job;
 import finder.service.FinderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,12 +9,10 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Optional;
 
 @EnableScheduling
 @Controller
@@ -25,6 +24,13 @@ public class FinderController {
 	public FinderController(SimpMessagingTemplate template, FinderService service) {
 		this.template = template;
 		this.service = service;
+	}
+
+	@GetMapping("/jobs/{id}")
+	public ResponseEntity<Job> job(@RequestAttribute String id) {
+		return service.getJob(id)
+					  .map(j -> new ResponseEntity<>(j, HttpStatus.OK))
+					  .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
 	}
 
 	@Scheduled(fixedRate = 4000)
