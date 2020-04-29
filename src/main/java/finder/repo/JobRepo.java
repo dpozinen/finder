1b09bdd3e987989adcf6beba3cf3bcc291040cb2
dpozinen <dpozinen@gmail.com>
2020-04-29 13:42:30 +0300

    nuke

--- src/main/java/finder/repo/JobRepo.java
+++
@@ -1,8 +0,0 @@
-package finder.repo;
-
-import finder.model.Job;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobRepo extends CrudRepository<Job, String> {}
